949574d86c1154cd19202da55ee86884ba077706
Ilmi <ilmirasyidah.sekawanmedia@gmail.com>
2023-12-01 11:32:09 +0700

    update home-page

--- src/views/home-page/HomePage.View.tsx
+++ src/views/home-page/HomePage.View.tsx
@@ -6,13 +6,18 @@ interface TypeView {
 
 const View = ({ navigate }: TypeView) => {
   return (
-    <main className="flex flex-col justify-center h-screen gap-10 place-items-center">
-      <h1 className="text-6xl font-extrabold">Code Foundation React Web App</h1>
+    <main className="flex flex-col justify-center h-screen gap-6 place-items-center">
+      <h1 className="mb-5 text-6xl font-extrabold">
+        Code Foundation React Web App
+      </h1>
+      <h3 className="text-sm">
+        This application was built using the following technology:
+      </h3>
       <h2 className="text-2xl font-medium ">
-        React + Vite + Typescript + Redux Toolkit (RTL Query) + tailwind CSS +
+        React + Vite + Typescript + Redux Toolkit (RTK Query) + tailwind CSS +
         i18n
       </h2>
-      <Button classNames="px-8 py-4 rounded-2xl" onClick={navigate}>
+      <Button classNames="px-8 py-4 rounded-2xl mt-5" onClick={navigate}>
         Get Start
       </Button>
     </main>
